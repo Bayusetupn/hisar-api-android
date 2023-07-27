46931cf353c56de8d6b0b0d61790fbbaddea0efc
Bayusetupn <22081010027@student.upnjatim.ac.id>
2023-07-27 21:29:46 +0700

    doc

--- controller/JamaahController.js
+++ controller/JamaahController.js
@@ -1,5 +1,6 @@
 import Jamaah from "../model/Jamaah.js";
 import Perkab from "../model/perlengkapan.js"
+import Dokumen from "../model/file.js"
 
 export const getPerkab = async(req,res)=>{
     try {
@@ -14,6 +15,28 @@ export const getPerkab = async(req,res)=>{
         }).catch(()=>{
             res.status(404).json({
                 message : "Perlengkapan Not Found!"
+            })
+        })
+    } catch (err) {
+        res.status(404).json({
+            message : "error " + err
+        })
+    }
+}
+
+export const getJamaahDoc = async(req,res)=>{
+    try {
+        await Dokumen.findOne({
+            where:{
+                jamaahId : req.body.id
+            }
+        }).then((result)=>{
+            res.status(200).json({
+                data : result
+            })
+        }).catch(err=>{
+            res.status(404).json({
+                message : "error " + err
             })
         })
     } catch (err) {
--- routes/jamaah.js
+++ routes/jamaah.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { auth_key, isLogin } from '../middlewate/auth.js'
-import { getPerkab } from '../controller/JamaahController.js'
+import { getJamaahDoc, getPerkab } from '../controller/JamaahController.js'
 
 const jamaah = express.Router()
 
 jamaah.post('/jamaah/perkab',auth_key,isLogin,getPerkab)
+jamaah.post('/jamaah/doc',auth_key,isLogin,getJamaahDoc)
 
 export default jamaah
