7558afd0d0913f9a0ffa80aa5e9e14bf0718985f
Bayusetupn <22081010027@student.upnjatim.ac.id>
2023-07-30 21:14:11 +0700

    edit dp

--- controller/JamaahController.js
+++ controller/JamaahController.js
@@ -45,3 +45,41 @@ export const getJamaahDoc = async(req,res)=>{
         })
     }
 }
+
+export const editDp = async(req,res)=>{
+    try {
+        await Jamaah.findOne({
+            where : {
+                id : req.body.id
+            }
+        }).then(async(result)=>{
+            
+            await Jamaah.update({
+                dp : true
+            },
+            {
+                where : {
+                    id : result.id
+                }
+            }
+            ).then(()=>{
+                res.status(200).json({
+                    message : "Sukses Edit Dp"
+                })
+            }).catch(()=>{
+                res.message(400).json({
+                    message : "Error Edit Dp"
+                })
+            })
+
+        }).catch(()=>{
+            res.status(404).json({
+                message : "Not Found!"
+            })
+        })
+    } catch (err) {
+        res.status(404).json({
+            message : "error"
+        })
+    }
+}
--- routes/jamaah.js
+++ routes/jamaah.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { auth_key, isLogin } from '../middlewate/auth.js'
-import { getJamaahDoc, getPerkab } from '../controller/JamaahController.js'
+import { editDp, getJamaahDoc, getPerkab } from '../controller/JamaahController.js'
 
 const jamaah = express.Router()
 
 jamaah.post('/jamaah/perkab',auth_key,isLogin,getPerkab)
 jamaah.post('/jamaah/doc',auth_key,isLogin,getJamaahDoc)
+jamaah.put('/jamaah/dp',auth_key,isLogin,editDp)
 
 export default jamaah
